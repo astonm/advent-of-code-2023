4eba6da5aa3d0d4e837d80ee3c6a8e9ceddad6de
Aston Motes <astonm@gmail.com>
2023-12-06 11:38:24 -0800

    add parameter to run other code

--- run.sh
+++ run.sh
@@ -2,7 +2,8 @@
 DAY="$1"
 PART="$2"
 INPUT=${3:-input}
-OTHER=${@:4}
+CODE=${4:-code}
+OTHER=${@:5}
 export PYTHONPATH="."
 export PIPENV_VERBOSITY=-1
-pipenv run python "$DAY/code.py" "$PART" "$DAY/$INPUT.txt" $OTHER
+pipenv run python "$DAY/$CODE.py" "$PART" "$DAY/$INPUT.txt" $OTHER
--- run_fast.sh
+++ run_fast.sh
@@ -2,7 +2,8 @@
 DAY="$1"
 PART="$2"
 INPUT=${3:-input}
-OTHER=${@:4}
+CODE=${4:-code}
+OTHER=${@:5}
 export PYTHONPATH="."
 export PIPENV_VERBOSITY=-1
-pypy3 "$DAY/code.py" "$PART" "$DAY/$INPUT.txt" $OTHER
+pypy3 "$DAY/$CODE.py" "$PART" "$DAY/$INPUT.txt" $OTHER
