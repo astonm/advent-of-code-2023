8d97abd11bad0fa52dcad74770e1f57002ff2c8d
Aston Motes <astonm@gmail.com>
2023-12-13 23:05:13 -0800

    clean up day12

--- day12/code.py
+++ day12/code.py
@@ -33,51 +33,14 @@ def part2(input):
         gs = g * repeats
 
         ways = count_ways(ps, gs)
-        print(p, g, ways)
         total += ways
-
     return submit.part(2, total)
 
 
-def count_ways(pattern, groupings):
-    pattern = re.sub(r"\.+", ".", pattern)  # multiple dots are as good as one
-    sub_patterns = pattern.split(".")
-    return do_it(sub_patterns, groupings)
-
-
-def do_it(sub_patterns, groupings, lvl=0):
-    if not sub_patterns:
-        return 1 if not groupings else 0
-
-    count = 0
-    sub_pattern = first(sub_patterns)
-    for j in range(0, len(groupings) + 1):
-        if len(sub_pattern) < sum(groupings[:j]):
-            break
-
-        curr_ways = inner_count_ways(sub_pattern, groupings[:j])
-        if curr_ways:
-            next_ways = do_it(sub_patterns[1:], groupings[j:], lvl=lvl + 1)
-
-            if next_ways:
-                count += curr_ways * next_ways
-    return count
-
-
 @lru_cache(maxsize=None)
-def inner_count_ways(pattern, groupings, lvl=0):
-    if sum(groupings) + len(groupings) - 1 > len(pattern):
-        return 0
+def count_ways(pattern, groupings, lvl=0):
     if not groupings:
-        if "#" in pattern:
-            return 0
-        else:
-            return 1
-    if len(groupings) == 1 and "#" not in pattern:
-        return max(0, len(pattern) - groupings[0] + 1)
-
-    if pattern.count("#") > sum(groupings):
-        return 0
+        return 0 if "#" in pattern else 1
 
     first_hash = pattern.find("#")
     if first_hash == -1:
@@ -86,6 +49,10 @@ def inner_count_ways(pattern, groupings, lvl=0):
     total = 0
     grouping = first(groupings)
     for i in range(min(first_hash, len(pattern) - grouping) + 1):
+        covered_pattern = pattern[i : i + grouping]
+        if "." in covered_pattern:
+            continue
+
         at_end = i + grouping == len(pattern)
         dot_ok = not at_end and pattern[i + grouping] != "#"
 
@@ -93,8 +60,8 @@ def inner_count_ways(pattern, groupings, lvl=0):
             total += 1
 
         if dot_ok:
-            res = inner_count_ways(pattern[i + grouping + 1 :], groupings[1:])
-            total += res
+            total += count_ways(pattern[i + grouping + 1 :], groupings[1:])
+
     return total
 
 
